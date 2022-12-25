3d9d1942580501dec1d87281aed9137253d0c8d0
ETOemperor <lumaochen@yeah.net>
2022-12-25 11:25:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<a href="http://etoemperor.github.io"></a>
+一个三体爱好者制作的网站↑
+会持续更新
---
+++ etothings/board/main.js
@@ -0,0 +1,7 @@
+while (true)
+{
+    setTimeout(100);
+    document.getElementById("1").innerHTML = "面壁计划";
+    document.getElementById("").innerHTML = "";
+    
+}
